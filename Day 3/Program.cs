f94762c0a97d0d40e1993ce2aa1372713f98a8c2
TheMoss <milena.k98@gmail.com>
2022-12-09 21:25:53 +0100

    Add solution for Day 3 puzzle

---
+++ Day 3/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_3;
+
+string[] lines = File.ReadAllLines(@"D:\Projects\AdventOfCode2022\Day 3\ListOfContents.txt");
+
+var lineCount = lines.Count();
+Rucksack firstRucksack = new Rucksack();
+
+
+foreach (string row in lines)
+{
+    var (firstHalf, secondHalf) = firstRucksack.GetHalves(row);
+
+    char[] firstHalfArray = firstHalf.ToCharArray();
+    char[] secondHalfArray = secondHalf.ToCharArray();
+
+    IEnumerable<char> intersectingItems = firstHalfArray.Intersect(secondHalfArray);
+    firstRucksack.GetPriority(intersectingItems);
+
+    firstRucksack.TotalFirstSum += firstRucksack.PartialSum;
+};
+
+Console.WriteLine("The sum of properties of those item types is {0}", firstRucksack.TotalFirstSum);
+
+Rucksack secondRucksack = new Rucksack();
+for (int i = 0; i < lineCount; i += 3)
+{
+    string firstLine = lines[i];
+    string secondLine = lines[i + 1];
+    string thirdLine = lines[i + 2];
+
+    HashSet<char> firstSet = new HashSet<char>(firstLine);
+    HashSet<char> secondSet = new HashSet<char>(secondLine);
+    HashSet<char> thirdSet = new HashSet<char>(thirdLine);
+
+    HashSet<char> commonSet = new HashSet<char>(firstSet.Intersect(secondSet));
+    HashSet<char> finalSet = new HashSet<char>(commonSet.Intersect(thirdSet));
+
+    secondRucksack.GetPriority(finalSet);
+
+    secondRucksack.TotalSecondSum += secondRucksack.PartialSum;
+
+}
+
+Console.WriteLine("The sum of properties of those item types is {0}", secondRucksack.TotalSecondSum);
+
+
+
---
+++ Day 3/Rucksack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    internal class Rucksack
+    {
+                
+        public int PartialSum { get; set; }
+        public int TotalFirstSum { get; set; }
+        public int TotalSecondSum { get; set; }
+
+
+        public (string firstHalf, string secondHalf) GetHalves(string row)
+        {
+            return
+                (
+                    row.Substring(0, (row.Length / 2)),
+                    row.Substring(row.Length / 2, row.Length / 2)
+                );
+
+        }
+
+
+        public int GetPriority(IEnumerable<char> intersectingItems)
+        {
+            PartialSum = intersectingItems.Select(c => Char.IsUpper(c) ? c - 38 : c - 96).Sum();
+            return PartialSum;
+        }
+
+
+    }
+}
