11d08b1109e9bd9d0aeb02a1b64406ce4d6c392d
TheMoss <milena.k98@gmail.com>
2022-12-23 00:48:08 +0100

    Add solution for Day 5 puzzle

---
+++ Day 5/ExpeditionColumns.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    internal class ExpeditionColumns
+    {
+        public string TopSeparateCrates { get; set; } = "";
+        public string TopTogetherCrates { get; set; } = "";      
+
+        public void MoveCratesSeparately(string[] input, List<Stack<char>> stackList)
+        {
+            foreach (string line in input)
+            {
+                string[] splitLine = line.Split(" ");//move,1,from,3,to,2
+                int itemAmount = int.Parse(splitLine[1]);
+                int sourceColumn = int.Parse(splitLine[3]);
+                int destinationColumn = int.Parse(splitLine[5]);
+
+                for (int i = 0; i < itemAmount; i++)
+                {
+                    stackList[destinationColumn - 1].Push(stackList[sourceColumn - 1].Pop());
+                }
+
+            }
+            TopSeparateCrates = string.Join("", stackList.Select(x => x.Peek()));
+        }
+
+        public void MoveCratesTogether(string[] input, List<Stack<char>> copyStackList)
+        {
+            
+            foreach(string line in input)
+            {
+                string[] splitLine = line.Split(" ");//move,1,from,3,to,2
+                int itemAmount = int.Parse(splitLine[1]);
+                int sourceColumn = int.Parse(splitLine[3]);
+                int destinationColumn = int.Parse(splitLine[5]);
+
+                var tempStack = new Stack<char>();
+                for (int j = 0; j < itemAmount; j++)
+                {
+                    tempStack.Push(copyStackList[sourceColumn -1].Pop());
+                }
+                while(tempStack.Count > 0)
+                {
+                    copyStackList[(destinationColumn -1)].Push(tempStack.Pop());
+                }
+            }
+            TopTogetherCrates = string.Join("", copyStackList.Select(x => x.Peek()));
+
+        }
+    }
+}
---
+++ Day 5/Program.cs
@@ -0,0 +1,39 @@
+﻿using Day_5;
+using Microsoft.Win32.SafeHandles;
+
+string[] input = File.ReadAllLines(@"D:\Projects\AdventOfCode2022\Day 5\CraneData.txt");
+var originalStackList = new List<Stack<char>>()
+{
+    new Stack<char>(new[] { 'G', 'T', 'R', 'W' }),
+    new Stack<char>(new[] { 'G', 'C', 'H', 'P', 'M', 'S', 'V', 'W' }),
+    new Stack<char>(new[] { 'C', 'L', 'T', 'S', 'G', 'M' }),
+    new Stack<char>(new[] { 'J', 'H', 'D', 'M', 'W', 'R', 'F' }),
+    new Stack<char>(new[] { 'P', 'Q', 'L', 'H', 'S', 'W', 'F', 'J' }),
+    new Stack<char>(new[] { 'P', 'J', 'D', 'N', 'F', 'M', 'S' }),
+    new Stack<char>(new[] { 'Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J' }),
+    new Stack<char>(new[] { 'R', 'T', 'B' }),
+    new Stack<char>(new[] { 'H', 'N', 'W', 'L', 'C' }),
+};
+
+var copyStackList = new List<Stack<char>>()
+{
+    new Stack<char>(new[] { 'G', 'T', 'R', 'W' }),
+    new Stack<char>(new[] { 'G', 'C', 'H', 'P', 'M', 'S', 'V', 'W' }),
+    new Stack<char>(new[] { 'C', 'L', 'T', 'S', 'G', 'M' }),
+    new Stack<char>(new[] { 'J', 'H', 'D', 'M', 'W', 'R', 'F' }),
+    new Stack<char>(new[] { 'P', 'Q', 'L', 'H', 'S', 'W', 'F', 'J' }),
+    new Stack<char>(new[] { 'P', 'J', 'D', 'N', 'F', 'M', 'S' }),
+    new Stack<char>(new[] { 'Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J' }),
+    new Stack<char>(new[] { 'R', 'T', 'B' }),
+    new Stack<char>(new[] { 'H', 'N', 'W', 'L', 'C' }),
+
+};
+
+ExpeditionColumns expedition = new ExpeditionColumns();
+
+expedition.MoveCratesSeparately(input, originalStackList);
+expedition.MoveCratesTogether(input, copyStackList);
+
+
+Console.WriteLine(@"Crates on top of the stacks when moved one by one:{0}", expedition.TopSeparateCrates);
+Console.WriteLine(@"Crates on top of the stacks when moved together:{0}", expedition.TopTogetherCrates);
