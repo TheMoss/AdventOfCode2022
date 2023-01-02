8bf16cf1c4072a8ae791b6a2c4a64ae95990b606
TheMoss <milena.k98@gmail.com>
2023-01-02 20:34:56 +0100

    Refactor Day 1 for better code reusability.

---
+++ Day 1/CaloriesPuzzle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    public class CaloriesPuzzle
+    {
+        public int Calories { get; set; }
+        public int MostCalories { get; set; }
+        public int TopThreeTotal { get; set; }
+        public List<int> CaloriesPerElf { get; set; } = new List<int>();
+
+        public void SumCaloriesPerElf(string filepath)
+        {
+            foreach (string line in System.IO.File.ReadLines(filepath))
+            {
+
+                if (!String.IsNullOrEmpty(line))
+                {
+                    int l = Int32.Parse(line);
+                    Calories += l;
+                }
+                else
+                {
+                    CaloriesPerElf.Add(Calories);
+                    Calories = 0;
+                }
+            }
+        }
+
+        public void GetMostCalories()
+        {
+            CaloriesPerElf.Sort();
+            CaloriesPerElf.Reverse();
+
+            MostCalories = CaloriesPerElf[0];
+        }
+         
+        public void SumTopThree()
+        {
+            TopThreeTotal = CaloriesPerElf[0] + CaloriesPerElf[1] + CaloriesPerElf[2];
+        }
+    }
+}
